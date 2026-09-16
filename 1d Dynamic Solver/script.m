fcebceac9ca3a0f386f09b1a1f72079d52dbe7f5
Daniyar-Omarov <150550870+Daniyar-Omarov@users.noreply.github.com>
2026-09-16 12:39:45 +0300

    Update script.m

--- 1d Dynamic Solver/script.m
+++ 1d Dynamic Solver/script.m
@@ -1,5 +1,5 @@
 clear; clc; Nt_list = [2; 4; 8]; N_list = [2; 4; 8; 16; 32; 64; 128];
-cost = zeros(7,3); err = zeros(7,3); timeval = zeros(7,3); Pi_final = cell(7,3);
+cost = zeros(7,3); err = zeros(7,3); timeval = zeros(7,3);
 
 cost_ex = 1/120; xa = 0; xb = 1; ya = 0; yb = 1; % Ex 1
 % cost_ex = (25/16)*asin(4/5)-17/12; xa = 0; xb = 1; ya = 0; yb = 1; % Ex 2
@@ -8,7 +8,7 @@ cost_ex = 1/120; xa = 0; xb = 1; ya = 0; yb = 1; % Ex 1
 
 for i=1:7
     for j=1:3
-        tic; [cost_val] = lin_min(Nt_list(j), N_list(i), N_list(i), xa, xb, ya, yb);
+        tic; cost_val = lin_min(Nt_list(j), N_list(i), N_list(i), xa, xb, ya, yb);
         timeval(i,j) = toc; cost(i,j) = cost_val(2); err(i,j) = abs(cost(i,j) - cost_ex);
     end
 end
