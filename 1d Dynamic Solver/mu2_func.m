d86cd40c1222d38bb26c44cdbd98c5ff34bd2f7c
Daniyar-Omarov <150550870+Daniyar-Omarov@users.noreply.github.com>
2026-09-16 12:34:23 +0300

    Update mu2_func.m

--- 1d Dynamic Solver/mu2_func.m
+++ 1d Dynamic Solver/mu2_func.m
@@ -4,12 +4,12 @@ function value = mu2_func(y)
 value = ones(size(y));
 
 % Example 2
-value = (3-2*y)/2;
+% value = (3-2*y)/2;
 
 % Example 3
-value = 1 - abs(y);
+% value = 1 - abs(y);
 
 % Example 4
-value = normpdf(y, -1, 0.5);
+% value = normpdf(y, -1, 0.5);
 
 end
