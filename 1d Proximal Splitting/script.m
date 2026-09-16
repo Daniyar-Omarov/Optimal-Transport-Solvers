a65958475e7bc73e681ef64393b263cf47d95a74
Daniyar-Omarov <150550870+Daniyar-Omarov@users.noreply.github.com>
2026-09-16 12:46:52 +0300

    Update script.m

--- 1d Proximal Splitting/script.m
+++ 1d Proximal Splitting/script.m
@@ -1,10 +1,10 @@
 clear; clc; Nt_list = [2; 4; 8]; N_list = [2; 4; 8; 16; 32; 64; 128];
 cost = zeros(7,3); timeval = zeros(7,3); err_cost = zeros(7,3);
 
-cost_ex = 1/120; xa = 0; xb = 1; ya = 0; yb = 1; ex_ind = 1; % Ex 1
-% cost_ex = (25/16)*asin(4/5)-17/12; xa = 0; xb = 1; ya = 0; yb = 1; ex_ind = 2; % Ex 2
-% cost_ex = 1/30; xa = -1; xb = 1; ya = -1; yb = 1; ex_ind = 3; % Ex 3
-% cost_ex = 1.25; xa = -10; xb = 10; ya = -10; yb = 10; ex_ind = 4; % Ex 4
+cost_ex = 1/120; xa = 0; xb = 1; ya = 0; yb = 1; % Ex 1
+% cost_ex = (25/16)*asin(4/5)-17/12; xa = 0; xb = 1; ya = 0; yb = 1; % Ex 2
+% cost_ex = 1/30; xa = -1; xb = 1; ya = -1; yb = 1; % Ex 3
+% cost_ex = 1.25; xa = -10; xb = 10; ya = -10; yb = 10; % Ex 4
 
 for i=1:7
     for j=1:3
