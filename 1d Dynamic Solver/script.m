128d269b9f14ff123d02694bdcd5b1731682445e
Daniyar-Omarov <150550870+Daniyar-Omarov@users.noreply.github.com>
2026-09-16 12:36:32 +0300

    Update script.m

--- 1d Dynamic Solver/script.m
+++ 1d Dynamic Solver/script.m
@@ -8,7 +8,7 @@ cost_ex = 1/120; xa = 0; xb = 1; ya = 0; yb = 1; % Ex 1
 % cost_ex = 1.25; xa = -10; xb = 10; ya = -10; yb = 10; % Ex 4
 
 for i=1:7
-    for j=1
+    for j=1:3
         tic; [cost_val] = lin_min(Nt_list(j), N_list(i), N_list(i), xa, xb, ya, yb);
         timeval(i,j) = toc; cost(i,j) = cost_val(2); err(i,j) = abs(cost(i,j) - cost_ex);
     end
